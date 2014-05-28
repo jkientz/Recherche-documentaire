90dc733695c8afdba63ca7ecff45bff77266a5e3
Kientz Jérémy <jkientz@etud.univ-angers.fr>
2014-05-28 14:17:01 +0200

    initial project import from KDevelop

---
+++ RechercheDocumentaire.cpp
@@ -0,0 +1,22 @@
+#include "RechercheDocumentaire.h"
+
+#include <QtGui/QLabel>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QAction>
+
+RechercheDocumentaire::RechercheDocumentaire()
+{
+    QLabel* l = new QLabel( this );
+    l->setText( "Hello World!" );
+    setCentralWidget( l );
+    QAction* a = new QAction(this);
+    a->setText( "Quit" );
+    connect(a, SIGNAL(triggered()), SLOT(close()) );
+    menuBar()->addMenu( "File" )->addAction( a );
+}
+
+RechercheDocumentaire::~RechercheDocumentaire()
+{}
+
+#include "RechercheDocumentaire.moc"
---
+++ RechercheDocumentaire.h
@@ -0,0 +1,14 @@
+#ifndef RechercheDocumentaire_H
+#define RechercheDocumentaire_H
+
+#include <QtGui/QMainWindow>
+
+class RechercheDocumentaire : public QMainWindow
+{
+Q_OBJECT
+public:
+    RechercheDocumentaire();
+    virtual ~RechercheDocumentaire();
+};
+
+#endif // RechercheDocumentaire_H
